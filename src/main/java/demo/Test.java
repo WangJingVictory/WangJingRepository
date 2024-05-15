8b1fa7d082badda9fd3344daaf923cb77ee223f6
zdpy123456 <wadpgmnljm@163.com>
2024-05-15 18:03:18 +0800

    5.15-1-Test

---
+++ src/main/java/demo/Test.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Test {
+    public static void main(String[] args) {
+        int i = 0, nCount = 0;
+        for(i=1; i<=5; i++)
+            nCount = nCount + i;
+    }
+}
